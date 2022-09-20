1f59604edc8c6e3b03abc98d9eadae538ed8e4e7
Trinh Trinh <6051071128@st.utc2.edu.vn>
2022-09-20 13:07:47 +0700

    Update title

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+//StrictMode trong local cảnh báo lỗi sớm và sẽ ma vào trong not tên root ở index.html
+//Đây là file đầu vào của project.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
